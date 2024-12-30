6aab7c57b4b1ecfa1ee45242385a1ccf5fec2817
Jean Clipperton <jeanl.clipperton@gmail.com>
2024-12-30 15:36:28 -0600

    add website

---
+++ static/slides/16-interactive-visualization/misc.R
@@ -0,0 +1,31 @@
+<!-- class: center, middle -->
+  
+  <!-- [https://bensoltoff.shinyapps.io/manager-survey/](https://bensoltoff.shinyapps.io/manager-survey/) -->
+  
+  <!-- ```{r echo = FALSE} -->
+  <!-- knitr::include_app("https://bensoltoff.shinyapps.io/manager-survey/", height ="550px") -->
+  <!-- ``` -->
+  
+  <!-- --- -->
+  
+  <!-- class: middle -->
+  
+  <!-- .large[.hand[livecoding]] -->
+  
+  <!-- .task[ -->
+                <!-- ```r -->
+                <!-- usethis::use_course("uc-dataviz/shiny") -->
+                <!-- ``` -->
+                
+                <!-- - Navigate to the `manager-survey-server` folder, and launch the app by opening the `app.R` file and clicking on *Run App*. This includes our completed UI plus starter code for the basic plots in the app. -->
+                
+                <!-- ] -->
+  
+  <!-- (See next slide for code developed during live coding session) -->
+  
+  <!-- --- -->
+  
+  <!-- class: middle -->
+  
+  <!-- The code for the app can be found [here](https://github.com/uc-dataviz/slides/blob/main/16-interactive-visualization/manager-survey/app.R). -->
+  
